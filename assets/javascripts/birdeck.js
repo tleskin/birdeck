b9cde17236a9e9ec722e7209fb5e0ebda203c354
Thomas Leskin <tleskin@gmail.com>
2015-06-17 13:48:38 -0600

    Refactor code and add fetch posts on button click

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,15 +1,37 @@
 $(document).ready(function(){
+  fetchPosts()
+  createPost()
+  fetchPostsButton()
+})
+
+
+function renderPost(postInfo) {
+  $("#latest-posts").append("" +
+  "<div class='post' data-id=" +
+  postInfo.id +
+  "><h6>Published on " +
+  postInfo.created_at +
+  "</h6><p>" +
+  postInfo.description +
+  "</p></div>")
+}
+
+function fetchPosts(){
   $.ajax({
     type: 'GET',
     url: 'https://turing-birdie.herokuapp.com/api/v1/posts.json',
     success: function(posts) {
       $.each(posts, function(index, post){
-        $("#latest-posts").append("<div class='post'><h6>Published on " +
-        post.created_at + "</h6><p>" + post.description + "</p></div>")
+        var newestPostID = parseInt($('.post').last().attr('data-id'))
+        if ( isNaN(newestPostID) || post.id > newestPostID) {
+          renderPost(post)
+        }
       })
     }
   })
+}
 
+function createPost(){
   $("#create-post").on('click', function(){
     var postParams = { post: { description: $("#post-description").val() }}
 
@@ -18,9 +40,14 @@ $(document).ready(function(){
       url: 'https://turing-birdie.herokuapp.com/api/v1/posts.json',
       data: postParams,
       success: function(newPost){
-        $("#latest-posts").append("<div class='post'><h6>Published on " +
-        newPost.created_at + "</h6><p>" + newPost.description + "</p></div>")
+        renderPost(newPost)
       }
     })
   })
-})
+}
+
+function fetchPostsButton(){
+  $("#button-fetch").on("click", function(){
+    fetchPosts()
+  })
+}
