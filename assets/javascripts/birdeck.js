323663ad7778e2389c45d3bd60a9f233c8b1d9a8
Thomas Leskin <tleskin@gmail.com>
2015-06-17 11:08:55 -0600

    Added AJAX to pull in posts from Birdie API

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'https://turing-birdie.herokuapp.com/api/v1/posts.json',
+    success: function(posts) {
+      $.each(posts, function(index, post){
+        $("#latest-posts").append("<div class='post'><h6>Published on " +
+        post.created_at + "</h6><p>" + post.description + "</p></div>")
+      })
+    }
+  })
+})
